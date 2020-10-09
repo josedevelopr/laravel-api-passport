82875c038e1cd43182594924ffeaf2100a61f77a
jose antonio alvino velasque <jose_antonio_alvino21@outlook.es>
2020-10-09 18:08:01 -0500

    created basic api and token management using passport

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ApiAuthController;
+use App\Http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => ['cors', 'json.response']], function(){
+    // Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+    // Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/login', [ApiAuthController::class, 'login'])->name('login.api');
+    Route::post('/register', [ApiAuthController::class, 'register'])->name('register.api');
+
+});
+
+Route::middleware('auth:api')->group(function(){
+    // Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+    Route::post('/logout', [ApiAuthController::class, 'logout'])->name('logout.api');
+});
